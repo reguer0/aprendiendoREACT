6111d6580a9ddc8d7394673701d6b5a3b778e49d
Gorka Reguero <reguerogorka@gmail.com>
2026-01-23 12:22:10 +0100

    gestion de errores

--- buscadordepeliculas/src/App.tsx
+++ buscadordepeliculas/src/App.tsx
@@ -4,11 +4,12 @@ import './App.css'
 import { FilmSearchBar } from './components/filmSearchBar';
 import { FilmCard } from './components/filmCard';
 import { useFilms } from './hooks/useFilms';
+import { ErrorCard } from './components/errorCard';
 
 function App() {
   const [filmName, setFilmName] = useState('Matrix');
   const [searchType, setSearchType] = useState('popular');
-  const { filmInfo, setfilmInfo } = useFilms(filmName, searchType);
+  const { filmInfo, setFilmInfo, error } = useFilms(filmName, searchType);
 
   const onsearch = (name: string) => {
     if (name) {
@@ -27,7 +28,7 @@ function App() {
     const sortedFilms = [...filmInfo].sort((a, b) => 
       ascending ? a.vote_average - b.vote_average : b.vote_average - a.vote_average
     );
-    setfilmInfo(sortedFilms);
+    setFilmInfo(sortedFilms);
   }
 
   return (
@@ -35,10 +36,15 @@ function App() {
       <h1>Buscador de Peliculas</h1>
       <FilmSearchBar onSearch={onsearch} filterByVotes={filterByVotes} onFilterChange={handleFilterChange} />
        
+    
+       
       <div className="films-container">
-        {filmInfo.map((film) => (
-          <FilmCard key={film.id} {...film} />
-        ))}
+        {error 
+        ? (<ErrorCard error={error} />)
+       :  (filmInfo.map((film) => (
+            <FilmCard key={film.id} {...film} />
+          )))
+        }
       </div>
     </>
   )
