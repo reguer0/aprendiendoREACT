f231444e92f50e1b7bc6d5924bce1e9f35d17603
Gorka Reguero <reguerogorka@gmail.com>
2026-01-26 11:49:11 +0100

    abrir trailer en yt clickando la foto

--- buscadordepeliculas/src/components/filmCard.tsx
+++ buscadordepeliculas/src/components/filmCard.tsx
@@ -1,8 +1,33 @@
 import type { FilmType } from '../types/types';
 import './filmCardStyle.css';
+import { useState, useEffect } from 'react';
+import { useTrailer } from '../hooks/useTrailer';
+
+
 export function FilmCard(film: FilmType) {
+    const [filmId, setFilmId] = useState<number | null>(null);
+
+    const filmTrailerInfo = useTrailer(filmId);
+
+    useEffect(() => {
+        if (filmTrailerInfo?.trailerInfo?.results?.length > 0 && filmId) {
+            const trailer = filmTrailerInfo?.trailerInfo?.results.find(
+                (video: any) => video.type === 'Trailer' && video.site === 'YouTube'
+            );
+            if (trailer?.key) {
+                const youtubeUrl = `https://www.youtube.com/watch?v=${trailer.key}`;
+                window.open(youtubeUrl, "_blank");
+                setFilmId(null);
+            }
+        }
+    }, [filmTrailerInfo.trailerInfo]);
+
+    const onFilmClick = (movieId: number) => {
+        setFilmId(movieId);
+    }
+
     return (
-        <div className="film-card">
+        <div className="film-card" >
             <h2>{film.title}</h2>
             <p>Release Date: {film.release_date}</p>
             
@@ -10,6 +35,11 @@ export function FilmCard(film: FilmType) {
                 <img 
                     src={`https://image.tmdb.org/t/p/w200${film.poster_path}`} 
                     alt={`Poster of ${film.title}`} 
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        onFilmClick(film.id);
+                    }}
+                    style={{ cursor: 'pointer' }}
                 />
             )}  
              <p className="rating">{film.vote_average} ({film.vote_count} votes)</p>         
